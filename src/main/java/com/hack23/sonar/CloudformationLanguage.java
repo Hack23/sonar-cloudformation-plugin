35026380a26584a29d44ec274f93e9312b56f87f
pether.sorling@gmail.com <pether.sorling@gmail.com>
2019-05-25 15:01:43 +0200

    cleanup

--- src/main/java/com/hack23/sonar/CloudformationLanguage.java
+++ src/main/java/com/hack23/sonar/CloudformationLanguage.java
@@ -31,54 +31,53 @@ import org.sonar.api.resources.AbstractLanguage;
  */
 public class CloudformationLanguage extends AbstractLanguage {
 
-	  /** The Constant NAME. */
-  	public static final String NAME = "Cloudformation";
-	  
-  	/** The Constant KEY. */
-  	public static final String KEY = "cfn";
+	/** The Constant NAME. */
+	public static final String NAME = "Cloudformation";
 
-	  /** The config. */
-  	private final Configuration config;
+	/** The Constant KEY. */
+	public static final String KEY = "cfn";
 
-	  /**
-  	 * Instantiates a new cloudformation language.
-  	 *
-  	 * @param config the config
-  	 */
-  	public CloudformationLanguage(final Configuration config) {
-	    super(KEY, NAME);
-	    this.config = config;
-	  }
+	/** The config. */
+	private final Configuration config;
 
-	  /**
-  	 * Gets the file suffixes.
-  	 *
-  	 * @return the file suffixes
-  	 */
-  	@Override
-	  public String[] getFileSuffixes() {
-	    String[] suffixes = filterEmptyStrings(config.getStringArray(CloudformationProperties.FILE_SUFFIXES_KEY));
-	    if (suffixes.length == 0) {
-	      suffixes = StringUtils.split(CloudformationProperties.FILE_SUFFIXES_DEFAULT_VALUE, ",");
-	    }
-	    return suffixes;
-	  }
+	/**
+	 * Instantiates a new cloudformation language.
+	 *
+	 * @param config the config
+	 */
+	public CloudformationLanguage(final Configuration config) {
+		super(KEY, NAME);
+		this.config = config;
+	}
 
-	  /**
-  	 * Filter empty strings.
-  	 *
-  	 * @param stringArray the string array
-  	 * @return the string[]
-  	 */
-  	private String[] filterEmptyStrings(final String[] stringArray) {
-	    final List<String> nonEmptyStrings = new ArrayList<String>();
-	    for (final String string : stringArray) {
-	      if (StringUtils.isNotBlank(string.trim())) {
-	        nonEmptyStrings.add(string.trim());
-	      }
-	    }
-	    return nonEmptyStrings.toArray(new String[nonEmptyStrings.size()]);
-	  }
+	/**
+	 * Gets the file suffixes.
+	 *
+	 * @return the file suffixes
+	 */
+	@Override
+	public String[] getFileSuffixes() {
+		String[] suffixes = filterEmptyStrings(config.getStringArray(CloudformationProperties.FILE_SUFFIXES_KEY));
+		if (suffixes.length == 0) {
+			suffixes = StringUtils.split(CloudformationProperties.FILE_SUFFIXES_DEFAULT_VALUE, ",");
+		}
+		return suffixes;
+	}
 
+	/**
+	 * Filter empty strings.
+	 *
+	 * @param stringArray the string array
+	 * @return the string[]
+	 */
+	private String[] filterEmptyStrings(final String[] stringArray) {
+		final List<String> nonEmptyStrings = new ArrayList<String>();
+		for (final String string : stringArray) {
+			if (StringUtils.isNotBlank(string.trim())) {
+				nonEmptyStrings.add(string.trim());
+			}
+		}
+		return nonEmptyStrings.toArray(new String[nonEmptyStrings.size()]);
+	}
 
 }
---
+++ src/test/java/com/hack23/sonar/CloudformationLanguageTest.java
@@ -0,0 +1,53 @@
+/*
+ * Cloudformation Plugin for SonarQube
+ * Copyright (C) 2019 James Pether Sörling
+ * james@hack23.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.hack23.sonar;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.sonar.api.config.Configuration;
+
+public class CloudformationLanguageTest extends Assert {
+
+	@Test
+	public void getFileSuffixesTest() {
+		final Configuration configuration = mock(Configuration.class);
+		when(configuration.getStringArray(CloudformationProperties.FILE_SUFFIXES_KEY)).thenReturn(new String[] {".template"});
+		
+		final CloudformationLanguage cloudformationLanguage = new CloudformationLanguage(configuration);
+		final String[] suffixes = cloudformationLanguage.getFileSuffixes();
+		assertEquals(".template",suffixes[0]);
+	}
+	
+	@Test
+	public void getFileSuffixesMultipleValuesTest() {
+		final Configuration configuration = mock(Configuration.class);
+		when(configuration.getStringArray(CloudformationProperties.FILE_SUFFIXES_KEY)).thenReturn(new String[] {".template",".yml"});
+		
+		final CloudformationLanguage cloudformationLanguage = new CloudformationLanguage(configuration);
+		final String[] suffixes = cloudformationLanguage.getFileSuffixes();
+		assertEquals(".template",suffixes[0]);
+		assertEquals(".yml",suffixes[1]);
+		
+	}
+
+}
