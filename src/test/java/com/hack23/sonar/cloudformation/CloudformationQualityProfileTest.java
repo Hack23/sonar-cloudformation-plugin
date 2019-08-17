b445bea289380a01ef94974470550952b842dce8
pether.sorling@gmail.com <pether.sorling@gmail.com>
2019-08-17 20:56:46 +0200

    W38 IOT policy should not allow * action 		W39 IoT policy should not allow * resource 		W40 Security Groups egress with an IpProtocol of -1 found 		W41 S3 Bucket should have encryption option set 		W42 Security Groups ingress with an ipProtocol of -1 found

--- src/test/java/com/hack23/sonar/cloudformation/CloudformationQualityProfileTest.java
+++ src/test/java/com/hack23/sonar/cloudformation/CloudformationQualityProfileTest.java
@@ -39,7 +39,7 @@ public class CloudformationQualityProfileTest extends Assert {
 		final BuiltInQualityProfile qualityProfile = context.profile(CloudformationLanguage.KEY,"Cloudformation Rules");
 		assertNotNull(qualityProfile);
 		assertTrue(qualityProfile.isDefault());
-		assertEquals(72,qualityProfile.rules().size());
+		assertEquals(77,qualityProfile.rules().size());
 		
 		
 	}
