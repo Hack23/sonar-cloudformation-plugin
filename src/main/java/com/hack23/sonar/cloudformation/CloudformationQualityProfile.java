f164d9e52e98f1975b106c3c706da38ad6066b2a
pether.sorling@gmail.com <pether.sorling@gmail.com>
2020-04-30 13:10:25 +0200

    prepare release of legacy version.

--- src/main/java/com/hack23/sonar/cloudformation/CloudformationQualityProfile.java
+++ src/main/java/com/hack23/sonar/cloudformation/CloudformationQualityProfile.java
@@ -107,6 +107,9 @@ public final class CloudformationQualityProfile implements BuiltInQualityProfile
 		SUPPORTED_RULES.add("F76");
 		SUPPORTED_RULES.add("F77");
 		SUPPORTED_RULES.add("F78");
+		SUPPORTED_RULES.add("F79");
+		SUPPORTED_RULES.add("F80");
+
 		SUPPORTED_RULES.add("F665");
 		SUPPORTED_RULES.add("F1000");
 		SUPPORTED_RULES.add("F2000");
@@ -173,6 +176,15 @@ public final class CloudformationQualityProfile implements BuiltInQualityProfile
 		SUPPORTED_RULES.add("W67");
 		SUPPORTED_RULES.add("W68");
 		SUPPORTED_RULES.add("W69");
+		SUPPORTED_RULES.add("W70");
+		SUPPORTED_RULES.add("W71");
+		SUPPORTED_RULES.add("W72");
+		SUPPORTED_RULES.add("W73");
+		SUPPORTED_RULES.add("W74");
+		SUPPORTED_RULES.add("W75");
+
+		SUPPORTED_RULES.add("W1200");
+		SUPPORTED_RULES.add("W1201");
 	}
 
 	public static boolean hasRule(final String id) {
--- src/test/java/com/hack23/sonar/cloudformation/CloudformationQualityProfileTest.java
+++ src/test/java/com/hack23/sonar/cloudformation/CloudformationQualityProfileTest.java
@@ -39,7 +39,7 @@ public class CloudformationQualityProfileTest extends Assert {
 		final BuiltInQualityProfile qualityProfile = context.profile(CloudformationLanguage.KEY,"Cloudformation Rules");
 		assertNotNull(qualityProfile);
 		assertTrue(qualityProfile.isDefault());
-		assertEquals(139,qualityProfile.rules().size());
+		assertEquals(149,qualityProfile.rules().size());
 	}
 
 }
